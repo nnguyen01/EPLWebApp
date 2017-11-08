cf865e80e14a67057c411658af5e8f13328b2d2d
will1029 <william-qw@hotmail.com>
2017-11-07 20:17:44 -0700

    Trying to fix routing so apache2 can host

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -7,33 +7,35 @@ import { AlertModule } from 'ngx-bootstrap';
 
 /* Components */
 import { AppRouterComponent } from './app-router.component';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+//import { LoginComponent } from './login/login.component';
+//import { DashboardComponent } from './dashboard/dashboard.component';
 
 /* Other routers (Child routers) */
-import { DashboardModule } from './dashboard/dashboard.module';
+//import { DashboardModule } from './dashboard/dashboard.module';
 
 /* All routes */
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, //Default route
-  { path: 'login',  component: LoginComponent },
-  { path: 'dashboard',  component: DashboardComponent },
+  //{ path: 'login',  component: LoginComponent },
+  //{ path: 'dashboard',  component: DashboardComponent },
+  
+  //{ path: '', redirectTo: '/login', pathMatch: 'full' }, //Default route
+  //{ path: '**', component: LoginComponent } //Wildcard route
 ];
- 
+
 @NgModule({
   /* Module imports here */
   imports: [
     AlertModule.forRoot(),
     BrowserModule,
-	DashboardModule,
-	RouterModule.forRoot(routes) //Adds the routes
+	//DashboardModule,
+	RouterModule.forRoot(routes, { enableTracing: true }) //Adds the routes
   ],
   /* Make routes viewable to those who import this module */
-  exports: [ RouterModule ],
+  //exports: [ RouterModule ],
   declarations: [
     AppRouterComponent,
 	//LoginComponent,
   ],
   bootstrap: [AppRouterComponent]
 })
-export class AppModule { }
+export class AppRouterModule { }
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 /* Set default entry point */
-import { AppModule } from './app/app-router.module';
+import { AppRouterModule } from './app/app-router.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppRouterModule)
   .catch(err => console.log(err));
