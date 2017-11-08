4e7e8989765d971f85e7a2d3dbcff0c053f04615
will1029 <william-qw@hotmail.com>
2017-11-07 22:40:53 -0700

    Remove some coe to test routing again

--- src/app/app-router.component.ts
+++ src/app/app-router.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  template: '<h1>Angular Router</h1><nav><a routerLink="/login" routerLinkActive="active">Heroes</a></nav><router-outlet></router-outlet>'
+  //templateUrl: './app.component.html'
 })
 export class AppRouterComponent {
   title = 'app';
--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -1,22 +1,21 @@
-import { NgModule }	from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }	from '@angular/core';
 import { RouterModule, Routes }   from '@angular/router';
 
-/* Root module */
 import { AlertModule } from 'ngx-bootstrap';
 
 /* Components */
 import { AppRouterComponent } from './app-router.component';
-//import { LoginComponent } from './login/login.component';
-//import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 /* Other routers (Child routers) */
-//import { DashboardModule } from './dashboard/dashboard.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 /* All routes */
 const routes: Routes = [
   //{ path: 'login',  component: LoginComponent },
-  //{ path: 'dashboard',  component: DashboardComponent },
+  { path: 'dashboard',  component: DashboardComponent },
   
   //{ path: '', redirectTo: '/login', pathMatch: 'full' }, //Default route
   //{ path: '**', component: LoginComponent } //Wildcard route
@@ -27,7 +26,7 @@ const routes: Routes = [
   imports: [
     AlertModule.forRoot(),
     BrowserModule,
-	//DashboardModule,
+	DashboardModule,
 	RouterModule.forRoot(routes, { enableTracing: true }) //Adds the routes
   ],
   /* Make routes viewable to those who import this module */
