c9b3c00ca989ec34958be9d08aadaef4f8d8cb76
nnguyen01 <Nathan.Ngn10@gmail.com>
2017-11-21 12:02:14 -0700

    Fixed dashboard-home component

--- src/app/components/dashboard/dashboard-home/dashboard-home.component.ts
+++ src/app/components/dashboard/dashboard-home/dashboard-home.component.ts
@@ -10,6 +10,13 @@ import { Branch } from '../../../models/branch';
     styleUrls: ['./dashboard-home.component.css']
 })
 export class DashboardHomeComponent implements OnInit {
+    /*
+    tiles = [
+        {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+        {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+        {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+        {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+      ]; */
     private branch: Branch;
     constructor() { }
     ngOnInit(): void {
