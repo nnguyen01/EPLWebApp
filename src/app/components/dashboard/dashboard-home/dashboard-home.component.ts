3547e116081eb762f9f75fd8d482c1e8eaf35add
nnguyen01 <Nathan.Ngn10@gmail.com>
2017-11-20 22:55:05 -0700

    Added getters and setters for branch model

--- src/app/components/dashboard/dashboard-home/dashboard-home.component.ts
+++ src/app/components/dashboard/dashboard-home/dashboard-home.component.ts
@@ -2,13 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
 
+import { Branch } from '../../../models/branch';
+
 @Component({
     selector: 'app-dashboard-home',
     templateUrl: './dashboard-home.component.html',
     styleUrls: ['./dashboard-home.component.css']
 })
 export class DashboardHomeComponent implements OnInit {
-    constructor(private router: Router, private auth: AuthService) { }
+    private branch: Branch;
+    constructor() { }
     ngOnInit(): void {
+        this.branch = new Branch("Clareview", 
+            "TestID12345678", 
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Melk_-_Abbey_-_Library.jpg/1200px-Melk_-_Abbey_-_Library.jpg")
     }
 }
--- src/app/models/branch.ts
+++ src/app/models/branch.ts
@@ -1,3 +1,27 @@
 export class Branch {
     constructor(name?: string, beaconID?: string, iLink?: string) { }
+
+    get name(): string {
+        return this.name;
+    }
+
+    set name(name: string) {
+        this.name = name;
+    }
+
+    get beaconID(): string {
+        return this.beaconID;
+    }
+
+    set beaconID(beaconID: string) {
+        this.beaconID = beaconID;
+    }
+
+    get iLink(): string {
+        return this.iLink;
+    }
+
+    set iLink(iLink: string) {
+        this.iLink = iLink;
+    }
 }
