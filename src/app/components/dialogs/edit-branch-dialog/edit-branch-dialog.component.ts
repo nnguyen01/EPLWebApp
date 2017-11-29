96e3324688741127bcd2ac566ea2ec3ef3762230
nnguyen01 <Nathan.Ngn10@gmail.com>
2017-11-29 16:57:05 -0700

    Added error messages

--- src/app/components/dialogs/edit-branch-dialog/edit-branch-dialog.component.ts
+++ src/app/components/dialogs/edit-branch-dialog/edit-branch-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { LibraryBranch } from '../../../models/library-branch';
 
@@ -12,12 +13,22 @@ import { MatIconRegistry } from '@angular/material';
 })
 export class EditBranchDialogComponent {
 
+    placeHolder: string;
+    branch: FormControl;
+
     constructor(public dialogRef: MatDialogRef<any>,
         @Inject(MAT_DIALOG_DATA) public library: LibraryBranch,
         private iconRegistry: MatIconRegistry,
         private sanitizer: DomSanitizer) {
         iconRegistry
             .addSvgIcon('edit',
-            sanitizer.bypassSecurityTrustResourceUrl('assets/img/edit-pencil.svg'))
+            sanitizer.bypassSecurityTrustResourceUrl('assets/img/edit-pencil.svg'));
+            this.branch = new FormControl('', [Validators.required, Validators.pattern(library.branch)])
     }
+
+    getErrorMessage() {
+        return (this.branch.hasError('required') && this.branch.touched) ? 'You must enter a name' :
+        (this.branch.dirty && this.branch.touched) ? 'Editing a branch name edits its questions and zones' :
+            '';
+  }
 }
