33be67b6b668fa1986e35c1954ecf0d040543dee
nnguyen01 <Nathan.Ngn10@gmail.com>
2017-11-30 01:51:52 -0700

    Added Create Branch Button

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { slideInDownAnimation } from '../../animations';
+
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
+
+import { MatRipple } from '@angular/material';
 
 @Component({
     selector: 'app-dashboard',
@@ -9,10 +14,19 @@ import { slideInDownAnimation } from '../../animations';
     styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+    @ViewChild(MatRipple) ripple: MatRipple;
     isLaunched: boolean = true;
     constructor(
         private router: Router,
-        private auth: AuthService) { }
+        private auth: AuthService,
+        private iconRegistry: MatIconRegistry,
+        private sanitizer: DomSanitizer) {
+        iconRegistry
+            .addSvgIcon('menu',
+            sanitizer.bypassSecurityTrustResourceUrl('assets/img/menu.svg'))
+            .addSvgIcon('add',
+            sanitizer.bypassSecurityTrustResourceUrl('assets/img/add.svg'));
+    }
 
     ngOnInit(): void {
         const token = localStorage.getItem('token');
